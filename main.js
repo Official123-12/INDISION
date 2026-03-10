b21578153a65cb3e6c0d24ed9f21d9b15ebc8828
Loving Programming <officialstanlee143@gmail.com>
2026-03-11 02:44:58 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+// main.js – Huanzisha Express server na Telegram bot kwa pamoja
+require('./index');      // Anzisha server ya Express (pairing, API)
+require('./telegramBot'); // Anzisha bot ya Telegram
