36f3a16ab767571c81a99cc7f51940ba50185ba9
Loving Programming <officialstanlee143@gmail.com>
2026-03-11 15:43:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
-require('./index');
-require('./telegramBot');
+require('dotenv').config(); // ✅ Load .env file first
+require('./index');         // Start Express server
+require('./telegramBot');   // Start Telegram bot
